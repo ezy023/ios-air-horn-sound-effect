776f43f3506d5a6620a9ecbdf30701ff16e992f2
Erik Allar <allareri@gmail.com>
2014-12-06 15:17:12 -0600

    Working with SystemSoundID, no support for iPhone6/6+

--- AirHorn/ViewController.m
+++ AirHorn/ViewController.m
@@ -9,19 +9,69 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    AVAudioPlayer *horn;
+    SystemSoundID airHorn;
+    UIButton *playSoundButton;
+}
+
+@property (nonatomic) SystemSoundID airHorn;
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    self.view.backgroundColor = [UIColor greenColor];
+    
+    NSString *airHornWAVPath = [[NSBundle mainBundle] pathForResource:@"convert-test.caf" ofType:nil];
+    NSURL *airHornURL = [NSURL fileURLWithPath:airHornWAVPath];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)airHornURL, &airHorn);
+    
+    [self addButtonToView];
+    [self addConstraintsToView];
+}
+
+- (void)dealloc
+{
+    AudioServicesDisposeSystemSoundID(_airHorn);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addButtonToView
+{
+    playSoundButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [playSoundButton setTitle:@"Play Sound" forState:UIControlStateNormal];
+
+    [playSoundButton addTarget:self action:@selector(playAirHornSound) forControlEvents:UIControlEventTouchDown];
+    [playSoundButton addTarget:self action:@selector(revertBackgroundColor) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:playSoundButton];
+    playSoundButton.translatesAutoresizingMaskIntoConstraints = NO;
+}
+
+- (void)playAirHornSound
+{
+    NSLog(@"Play the Air Horn Sound Now");
+    AudioServicesPlaySystemSound(airHorn);
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)revertBackgroundColor
+{
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)addConstraintsToView
+{
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playSoundButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:playSoundButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]];
 }
 
 @end
